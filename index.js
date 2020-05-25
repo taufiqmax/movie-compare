423ad11c1ffdec515e85cecf109bd1821cd285a2
Md Taufiqur Rahman <taufiqmax@gmail.com>
2020-05-25 22:34:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const autoCompleteConfig = {
     return movie.Title;
   },
   async fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
       params: {
         apikey: 'a330ecee',
         s: searchTerm,
@@ -47,7 +47,7 @@ createAutoComplete({
 let leftMovie;
 let rightMovie;
 const onMovieSelect = async (movie, summaryElement, side) => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+  const response = await axios.get('https://www.omdbapi.com/', {
     params: {
       apikey: 'a330ecee',
       i: movie.imdbID,
